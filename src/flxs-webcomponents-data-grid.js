85035ebc6d09df30e23b63f9cbd5821867c52059
Flexicious <info@flexicious.com>
2017-03-12 18:13:31 -0400

    Initial Commit

---
+++ src/flxs-webcomponents-data-grid.js
@@ -0,0 +1,82 @@
+(function () {
+    var template = new flexiciousNmsp.FlexDataGrid();
+    var gridPropsAndBehaviors = flexiciousNmsp.SettingsParser.getPropertiesAndBehaviors(template);
+    var behaviors = gridPropsAndBehaviors.behaviors, properties = gridPropsAndBehaviors.properties;
+
+    Polymer({
+        is: 'flxs-webcomponents-data-grid',
+        properties: properties,
+        behaviors: [
+            behaviors
+        ],
+
+        /**
+         * @property grid
+         * @type FlexiDataGrid
+         * @default null
+         */
+        grid: null,
+
+        /**
+         * On create element but not attached to DOM
+         */
+        created: function () {
+
+        },
+
+        /**
+         * On attached element to DOM
+         */
+        attached: function () {
+            if (this.width) {
+                this.$.gridContainer.style.width = this.width;
+            }
+            if (this.height) {
+                this.$.gridContainer.style.height = this.height;
+            }
+
+            this.grid = new flexiciousNmsp.FlexDataGrid(this.$.gridContainer);
+            this.async(function () {
+                for (var key in properties) {
+                    if (this[key.toLowerCase()]) {
+                        if (key === "style") {
+                            //this is thehtmlstyle
+                        } else {
+                            this.grid.applyAttribute(this.grid, properties[key].orig, this[key.toLowerCase()], true);
+                        }
+                    }
+                }
+
+                if(this.grid.getColumnLevel()._tempCols.length){
+                    this.grid.getColumnLevel().setColumns(this.grid.getColumnLevel()._tempCols);
+                }
+            }, 1);
+        },
+
+        /**
+         * Try to destroy handsontable instance if hadn't been destroyed
+         */
+        detached: function () {
+
+        },
+        ready: function () {
+        },
+
+        /**
+         * Register hooks
+         */
+        registerHooks: function () {
+
+        },
+
+
+        attributeChanged: function () {
+            this._onChanged();
+        },
+
+        _onChanged: function () {
+            // var settings;
+            //TODO - dynamic updates
+        }
+    });
+} ());
